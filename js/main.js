cf0d5b481b8005ba05b043132ed5c30f8af3083b
MerakchiHibaa <ninamimi305@gmail.com>
2021-12-13 21:18:04 +0100

    finished the doll

---
+++ js/main.js
@@ -0,0 +1,94 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+renderer.setClearColor( 0xb7c3f3 , 1 ) ; 
+const light = new THREE.AmbientLight(0xffffff) ; 
+scene.add(light) ; 
+/* const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube ); */
+
+//global variabes 
+const start_position = 3 
+const end_position = -start_position
+
+
+function createCube (size , positionX , rotY = 0 , color = 0xfbc851) {
+    const geometry = new THREE.BoxGeometry(size.w , size.h , size.d);
+    const material = new THREE.MeshBasicMaterial( { color: color } );
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.x = positionX ; 
+    cube.rotation.y = rotY ; 
+    scene.add( cube ); 
+    return cube;
+
+}
+
+camera.position.z = 5; //the smaller the number the closer the camera is to the object
+
+const loader = new THREE.GLTFLoader();
+
+class Doll {
+    constructor() {
+
+        
+loader.load('../models/scene.gltf' , (gltf) => {
+    //we use an arrow function because a regular function will have its own context and it this.doll will refer to the function.doll and not the object doll
+    scene.add(gltf.scene) ;
+    gltf.scene.scale.set(.4 , .4 , .4) ; 
+    gltf.scene.position.set (0, -1 , 0)
+    this.doll = gltf.scene ; 
+
+}) ; 
+
+        
+    }
+
+    lookBackward() {
+       // this.doll.rotation.y = -3.15 ; 
+       gsap.to(this.doll.rotation , {y: -3.15 , duration: .45}) ;
+    }
+    lookForward() {
+    //    this.doll.rotation.y = 0 ; 
+    gsap.to(this.doll.rotation , {y: 0 , duration: .45});
+    }
+
+}
+
+function createTrack() {
+    createCube( {w: start_position *2 + 0.2 ,h: 1.5 ,d: 1} , 0 , 0, 0xe5a716).position.z = - 1;
+
+    createCube( {w:.2 ,h: 1.5 ,d: 1} , start_position , - .35 ) ;
+    createCube( {w:.2 ,h: 1.5 ,d: 1} , end_position , .35) ;
+
+
+
+}
+createTrack() ;
+
+let doll = new Doll() ; 
+setTimeout(() => {
+    doll.lookBack()
+} , 1000 ) ; 
+
+
+function animate() {
+    renderer.render(scene , camera) ;
+
+	requestAnimationFrame( animate );
+ }
+animate();
+//to make it responsive
+window.addEventListener('resize' , onWindowResize , false) ; 
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight ; 
+    camera.updateProjectionMatrix() ; 
+    renderer.setSize(window.innerWidth , window.innerHeight) ; 
+
+}
